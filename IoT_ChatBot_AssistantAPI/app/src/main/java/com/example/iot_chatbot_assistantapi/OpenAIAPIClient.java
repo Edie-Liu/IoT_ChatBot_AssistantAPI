013357f21d83b9909f4cdfd6aa9654fb48213072
Edie <lht.eydi@gmail.com>
2024-08-01 11:07:51 +0800

    OpenAIAPIClient.java Update assistants version

--- IoT_ChatBot_AssistantAPI/app/src/main/java/com/example/iot_chatbot_assistantapi/OpenAIAPIClient.java
+++ IoT_ChatBot_AssistantAPI/app/src/main/java/com/example/iot_chatbot_assistantapi/OpenAIAPIClient.java
@@ -29,27 +29,27 @@ public class OpenAIAPIClient {
     public interface OpenAIAPIService {
 
         // 定義創建助手的POST請求
-        @Headers("OpenAI-Beta: assistants=v1")
+        @Headers("OpenAI-Beta: assistants=v2")
         @POST("assistants")
         Call<ResponseBody> createAssistant(@Body OpenAIAssistantRequestModel createRequestModel);
 
         // 定義刪除助手的DELETE請求
-        @Headers("OpenAI-Beta: assistants=v1")
+        @Headers("OpenAI-Beta: assistants=v2")
         @DELETE("assistants/{assistant_id}")
         Call<Void> deleteAssistant(@Path("assistant_id") String assistantId);
 
         // 定義獲取對話線程的POST請求
-        @Headers("OpenAI-Beta: assistants=v1")
+        @Headers("OpenAI-Beta: assistants=v2")
         @POST("threads")
         Call<ResponseBody> getThreads();
 
         // 定義刪除對話線程的DELETE請求
-        @Headers("OpenAI-Beta: assistants=v1")
+        @Headers("OpenAI-Beta: assistants=v2")
         @DELETE("threads/{thread_id}")
         Call<Void> deleteThread(@Path("thread_id") String assistantId);
 
         // 定義發送消息到對話線程的POST請求
-        @Headers("OpenAI-Beta: assistants=v1")
+        @Headers("OpenAI-Beta: assistants=v2")
         @POST("threads/{thread_id}/messages")
         Call<ResponseBody> postMessageToThread(
                 @Path("thread_id") String threadId,
@@ -57,7 +57,7 @@ public class OpenAIAPIClient {
         );
 
         // 定義創建運行請求的POST請求
-        @Headers("OpenAI-Beta: assistants=v1")
+        @Headers("OpenAI-Beta: assistants=v2")
         @POST("threads/{thread_id}/runs")
         Call<ResponseBody> createRun(
                 @Path("thread_id") String threadId,
@@ -65,7 +65,7 @@ public class OpenAIAPIClient {
         );
 
         // 定義獲取運行結果的GET請求
-        @Headers("OpenAI-Beta: assistants=v1")
+        @Headers("OpenAI-Beta: assistants=v2")
         @GET("threads/{thread_id}/runs/{run_id}")
         Call<ResponseBody> getRun(
                 @Path("thread_id") String threadId,
@@ -73,7 +73,7 @@ public class OpenAIAPIClient {
         );
 
         // 定義獲取對話消息的GET請求
-        @Headers("OpenAI-Beta: assistants=v1")
+        @Headers("OpenAI-Beta: assistants=v2")
         @GET("threads/{thread_id}/messages?order=desc&limit=1")
         Call<ResponseBody> getMessage(
                 @Path("thread_id") String threadId
